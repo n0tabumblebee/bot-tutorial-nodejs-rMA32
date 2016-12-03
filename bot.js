fcd4f2c47c223bd7d5442f536c9b1c2dd66cfde9
n0tabumblebee <n0tabumblebee@yahoo.com>
2016-12-02 18:55:31 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ function respond() {
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
-                "MIA","BUF","SF","WAS","NYJ","TB"]
+                "MIA","BKN","SF","WAS","NYJ","TB"]
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage(cool());
